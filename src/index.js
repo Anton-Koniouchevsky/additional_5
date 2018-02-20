5da246b2a155a5de6a430b7550cc50d2ebc59df8
Anton-Koniouchevsky <anton.koniouchevsky@gmail.com>
2018-02-20 11:55:28 +0300

    Add check function implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let bracketsStack = [];
+    let strArray = str.split('');
+    for (let i = 0; i < strArray.length; i++) {
+        let configIndex;
+        bracketsConfig.forEach((config, index) => {
+            if (~config.indexOf(strArray[i])) configIndex = index;
+        });
+        if (bracketsStack[bracketsStack.length - 1] == bracketsConfig[configIndex][0]) {
+            if (strArray[i] == bracketsConfig[configIndex][1]) bracketsStack.pop();
+            else bracketsStack.push(strArray[i]);
+        } else {
+            if (strArray[i] == bracketsConfig[configIndex][0]) bracketsStack.push(strArray[i]);
+            else return false;
+        };
+    }
+    return !bracketsStack.length;
 }
